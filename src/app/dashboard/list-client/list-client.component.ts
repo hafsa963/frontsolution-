725bd102b54aa437f6b7acc67a5849a3f8ae49f2
badrmeliani <melianibadr9@gmail.com>
2023-12-07 09:41:54 +0100

    save

--- src/app/dashboard/list-client/list-client.component.ts
+++ src/app/dashboard/list-client/list-client.component.ts
@@ -455,7 +455,18 @@ window.onload = () => {
 };
 }
 
-
+downloadFile(fileName: string): void {
+  this.attachment.downloadFile(fileName).subscribe((blob: Blob) => {
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  });
+}
  
 
 }
